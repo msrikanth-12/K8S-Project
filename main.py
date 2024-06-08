df59cc0d3fb4ac676553f5b025d57388daead249
root <root@ip-172-31-49-189.ec2.internal>
2024-06-08 22:08:50 +0000

    main.py udpate

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-#templates = Jinja2Templates(directory="/code")
-templates = Jinja2Templates(directory="C:/Users/Lenovo/Desktop/k8_Project")
+templates = Jinja2Templates(directory="/code")
+#templates = Jinja2Templates(directory="C:/Users/Lenovo/Desktop/k8_Project")
 
-@app.get("/mypage")
+@app.get("/")
 def form_post(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
